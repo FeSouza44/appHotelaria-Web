46d377aed1b30fb0177f7c10da3a477c12b8c201
FELIPE AMARAL DE SOUZA <felipe.asouza29@senacsp.edu.br>
2025-08-05 17:39:12 -0300

    nodejs

---
+++ src/components/LoginForm.js
@@ -0,0 +1,26 @@
+
+export default function LoginForm() {
+    const formulario = document.createElement
+    ('form');
+    
+    const email = document.createElement
+    ('input');
+    email.className = 'input';
+    email.type = 'email';
+    email.placeholder = "Digite seu e-mail";
+    formulario.appendChild(email);
+
+    const senha = document.createElement('input');
+    senha.className = 'input';
+    senha.type = 'password';
+    senha.placeholder = "Digite sua senha: ";
+    formulario.appendChild(senha);
+
+    const entrar = document.createElement('button');
+    entrar.type = 'submit';
+    entrar.className = 'input';
+    entrar.textContent = "Entrar";
+    formulario.appendChild(entrar);
+    return formulario;
+
+}
---
+++ src/pages/login.js
@@ -0,0 +1,15 @@
+import LoginForm from "../components/LoginForm.js";
+
+export default function renderLoginPage() {
+    const divRoot = document.getElementById('root');
+    divRoot.innerHTML = '';
+
+    const container = document.createElement('div');
+    container.className = 'card p-4 shadow-lg';
+    container.style.width = '100%'; //Container com 100% de largura na div para ocupar a tela 
+    container.style.maxWidth = '400px'; // Até que atinja o limite de 400px
+    divRoot.appendChild(container);
+
+    const formulario = LoginForm();
+    container.appendChild(formulario);
+}
